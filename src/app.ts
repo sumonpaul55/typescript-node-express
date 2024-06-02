d6234c431ce048172a88c311bc6da2f4fc10dea2
sumonpaul55 <sumpaul100@gmail.com>
2024-06-02 21:37:59 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { StudentRoute } from "./app/modules/student/student.route";
 import { UserRoute } from "./app/modules/user/user.route";
+import globalErrorHandler from "./app/middleWare/globalErrorHandler";
 
 const app: Application = express();
 // user cors
@@ -16,15 +17,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello mongoose wolrd");
 });
 
-// global error handler
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  let statusCode = 500;
-  let message = "Something went wrong";
-  return res.status(statusCode).json({
-    success: false,
-    message: err.message || message,
-    error: err,
-  });
-});
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/app/middleWare/globalErrorHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import app from "../../app";
+
+// global error handler
+const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let message = "Something went wrong";
+  return res.status(statusCode).json({
+    success: false,
+    message: err.message || message,
+    error: err,
+  });
+};
+
+export default globalErrorHandler;
