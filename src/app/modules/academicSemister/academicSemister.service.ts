b2894e4e19edeef8b36d28c3fed888c5d65d56b1
sumonpaul55 <sumpaul100@gmail.com>
2024-06-07 23:58:08 +0600

    semister code and name mathing using maper object in academic semister service

--- src/app/modules/academicSemister/academicSemister.service.ts
+++ src/app/modules/academicSemister/academicSemister.service.ts
@@ -2,6 +2,23 @@ import { TAcademicSemister } from "./academicSemisterInterFace";
 import { AcademicSemister } from "./academicSemisterModel";
 
 const createAcademicSemisterDb = async (payLoad: TAcademicSemister) => {
+  // semister name ------> semister ocde
+  type TAcademicSemister = {
+    [Key: string]: string;
+    // Autumn: "01";
+    // Summer: "02";
+    // Fall: "03";
+  };
+  const academicSemisterNameCodeMapper: TAcademicSemister = {
+    Autumn: "01",
+    Summer: "02",
+    Fall: "03",
+  };
+
+  // check the semister name to code
+  if (academicSemisterNameCodeMapper[payLoad.name] !== payLoad.code) {
+    throw new Error("Does not matching with semister name and code");
+  }
   const result = await AcademicSemister.create(payLoad);
   return result;
 };
